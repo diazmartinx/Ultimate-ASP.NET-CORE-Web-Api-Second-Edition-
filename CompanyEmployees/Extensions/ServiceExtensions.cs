693eb2f2828d2b4beb83781b6ec46fd814b1ac98
Martin Diaz <midiazgt@gmail.com>
2022-12-06 00:31:06 -0300

    .

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,5 +1,9 @@
 ﻿using Contracts;
 using LoggerService;
+using Repository;
+using Service.Contracts;
+using Service;
+using Microsoft.EntityFrameworkCore;
 
 public static class ServiceExtensions
 {
@@ -19,5 +23,13 @@ public static class ServiceExtensions
     
     public static void ConfigureLoggerService(this IServiceCollection services) =>
         services.AddSingleton<ILoggerManager, LoggerManager>();
-
+    
+    public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+        services.AddScoped<IRepositoryManager, RepositoryManager>();
+    public static void ConfigureServiceManager(this IServiceCollection services) =>
+        services.AddScoped<IServiceManager, ServiceManager>();
+    public static void ConfigureSqlContext(this IServiceCollection services,
+        IConfiguration configuration) =>
+        services.AddDbContext<RepositoryContext>(opts =>
+        opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 }
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.HttpOverrides;
 using NLog;
-//43
+//64
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +9,9 @@ LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nl
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
 
 // Add services to the container.
 
